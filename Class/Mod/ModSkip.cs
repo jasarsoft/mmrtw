1f19d6ae31b78ed8e94a7b63ef533db514662b56
jasarsoft <edinjasar14@gmail.com>
2016-12-18 22:38:19 +0100

    new mod skip class

---
+++ Class/Mod/ModSkip.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Jasarsoft.ModManager.RTW
+{
+    public sealed class ModSkip
+    {
+        string[] names;
+
+        //constructor
+        public ModSkip()
+        {
+            names = new string[] 
+            { 
+                "alexander",
+                "bi",
+                "data",
+                "manager",
+                "miles",
+                "preferences",
+                "saves",
+                "temp",
+                "uninstall"
+            };
+        }
+
+        //property
+        public string[] Names
+        {
+            get { return names; }
+        }
+    }
+}
