3072debb9a1440b1f72e0fa66e087306d098b4b1
jasarsoft <edinjasar14@gmail.com>
2016-12-20 19:48:16 +0100

    new mod path class

---
+++ Class/Mod/ModPath.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jasarsoft.ModManager.RTW
+{
+    public sealed class ModPath
+    {
+        //field
+        private string path;
+
+        //constructor
+        public ModPath()
+        {
+            path = Environment.CurrentDirectory + @"\mods\";
+        }
+
+        //property
+        public string Path
+        {
+            get { return path; }
+        }
+    }
+}
