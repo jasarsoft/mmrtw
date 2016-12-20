c5ef636bf2cd5272807df6260d3a7ea401111e1c
jasarsoft <edinjasar14@gmail.com>
2016-12-20 21:09:44 +0100

    new game path class

---
+++ Class/Game/GamePath.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jasarsoft.ModManager.RTW
+{
+    public sealed class GamePath
+    {
+        //field
+        private string path;
+
+        //constructor
+        public GamePath()
+        {
+            path = Environment.CurrentDirectory;
+
+            if (path.EndsWith("manager"))
+                path = path.Substring(0, path.Length - 7);
+            else
+                path = @"..\";
+        }
+
+
+        //property
+        public string Path
+        {
+            get { return path; }
+        }
+
+
+        //method
+        public bool Valid()
+        {
+            if (path == @"..\")
+                return false;
+
+            return true;
+        }
+    }
+}
