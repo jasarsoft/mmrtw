664da29afceb6672520d3d9df21f26059b45906a
jasarsoft <edinjasar14@gmail.com>
2016-12-20 23:18:17 +0100

    new option file class

---
+++ Class/Option/OptionFile.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Jasarsoft.ModManager.RTW
+{
+    public sealed class OptionFile
+    {
+        private const string name = "options.xml";
+
+
+        public OptionFile()
+        {
+            //null;
+        }
+
+
+        public string Name
+        {
+            get { return name; }
+        }
+    }
+}
