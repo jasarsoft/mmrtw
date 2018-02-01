9c6ad13dcab6f429cb3ff4bf060fc08b2c096b25
Edin Jašarević <edinjasar14@gmail.com>
2018-02-01 22:28:56 +0100

    paths the game

---
+++ Rome Total War/Class/Game/GamePath.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jasarsoft.ModManager.RomeTotalWar
+{
+    internal sealed class GamePath
+    {
+        private readonly string pathRTW;
+        private readonly string pathRTWBI;
+        private readonly string pathRTWALX;
+
+
+        public GamePath()
+        {
+            this.pathRTW = @"\data";
+            this.pathRTWBI = @"\bi\data";
+            this.pathRTWALX = @"\alexander\data";
+        }
+
+
+        public string PathRTW
+        {
+            get { return this.pathRTW; }
+        }
+
+        public string PathRTWBI
+        {
+            get { return this.pathRTWBI; }
+        }
+
+        public string PathRTWALX
+        {
+            get { return this.pathRTWALX; }
+        }
+    }
+}
