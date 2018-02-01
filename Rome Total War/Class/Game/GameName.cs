0d5c8aad89ee90b355ef5a40bbfb5b32a5943b90
Edin Jašarević <edinjasar14@gmail.com>
2018-02-01 22:28:27 +0100

    names the game

---
+++ Rome Total War/Class/Game/GameName.cs
@@ -0,0 +1,33 @@
+﻿
+namespace Jasarsoft.ModManager.RomeTotalWar
+{
+    internal sealed class GameName
+    {
+        private readonly string nameRTW;
+        private readonly string nameRTWBI;
+        private readonly string nameRTWALX;
+
+        public GameName()
+        {
+            this.nameRTW = "RomeTW.exe";
+            this.nameRTWBI = "RomeTW-BI.exe";
+            this.nameRTWALX = "RomeTW-ALX.exe";
+        }
+
+
+        public string NameRTW
+        {
+            get { return this.nameRTW; }
+        }
+
+        public string NameRTWBI
+        {
+            get { return this.nameRTWBI; }
+        }
+
+        public string NameRTWALX
+        {
+            get { return this.nameRTWALX; }
+        }
+    }
+}
