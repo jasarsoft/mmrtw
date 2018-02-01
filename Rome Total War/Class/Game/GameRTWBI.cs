62c489fb3f276df37a7c5dc143e3e2b07594f530
Edin Jašarević <edinjasar14@gmail.com>
2018-02-01 22:34:47 +0100

    rtwbi game

---
+++ Rome Total War/Class/Game/GameRTWBI.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jasarsoft.ModManager.RomeTotalWar
+{
+    internal sealed class GameRTWBI : GameTW
+    {
+        public GameRTWBI() : base()
+        {
+            GameName name = new GameName();
+            GamePath path = new GamePath();
+            GameTitle title = new GameTitle();
+
+            this.Name = name.NameRTWBI;
+            this.Path = path.PathRTWBI;
+            this.Type = GameType.GameRTWBI;
+            this.Title = title.TitleRTWBI;
+            this.Size = GetSize();
+            this.Version = GetVersion();
+        }
+    }
+}
