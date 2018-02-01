9e57c19c21c519a4d13a66e117013f496a5c3d1b
Edin Jašarević <edinjasar14@gmail.com>
2018-02-01 22:43:12 +0100

    rtw game

---
+++ Rome Total War/Class/Game/GameRTW.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Jasarsoft.ModManager.RomeTotalWar
+{
+    internal sealed class GameRTW : GameTW
+    {
+        public GameRTW() : base()
+        {
+            GameName name = new GameName();
+            GamePath path = new GamePath();
+            GameTitle title = new GameTitle();
+
+            this.Name = name.NameRTW;
+            this.Path = path.PathRTW;
+            this.Type = GameType.GameRTW;
+            this.Title = title.TitleRTW;
+            this.Size = GetSize();
+            this.Version = GetVersion();
+        }
+    }
+}
