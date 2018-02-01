f3828f3462cfa68bd3bda018ba85f89fe81178a7
Edin Jašarević <edinjasar14@gmail.com>
2018-02-01 22:27:07 +0100

    type of game

---
+++ Rome Total War/Class/Game/GameType.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Jasarsoft.ModManager.RomeTotalWar
+{
+    enum GameType
+    {
+        GameTW,     //None
+        GameRTW,    //Rome Total War
+        GameRTWBI,  //Rome Total War - Barbarian Invasion
+        GameRTWALX  //Rome Total War - Alexander
+    };
+}
