632f061af02d347047a32a6d45c40fe16547a62a
Edin Jašarević <edinjasar14@gmail.com>
2018-02-01 22:31:41 +0100

    titles of game

---
+++ Rome Total War/Class/Game/GameTitle.cs
@@ -0,0 +1,33 @@
+﻿
+namespace Jasarsoft.ModManager.RomeTotalWar
+{
+    internal sealed class GameTitle
+    {
+        private readonly string titleRTW;
+        private readonly string titleRTWBI;
+        private readonly string titleRTWALX;
+        
+        public GameTitle()
+        {
+            this.titleRTW = "Rome: Total War™";
+            this.titleRTWBI = "Rome: Total War™ - Barbarian Invasion";
+            this.titleRTWALX = "Rome: Total War™ - Alexander";
+        } 
+
+
+        public string TitleRTW
+        {
+            get { return this.titleRTW; }
+        }
+
+        public string TitleRTWBI
+        {
+            get { return this.titleRTWBI; }
+        }
+
+        public string TitleRTWALX
+        {
+            get { return this.titleRTWALX; }
+        }
+    }
+}
