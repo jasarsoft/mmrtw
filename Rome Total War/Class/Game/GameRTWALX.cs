0bb1979957912855fa6e5c76eaa9d5c22481c472
Edin Jašarević <edinjasar14@gmail.com>
2018-02-01 22:45:12 +0100

    rtwalx game

---
+++ Rome Total War/Class/Game/GameRTWALX.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jasarsoft.ModManager.RomeTotalWar
+{
+    internal sealed class GameRTWALX : GameTW
+    {
+        public GameRTWALX() : base()
+        {
+            GameName name = new GameName();
+            GamePath path = new GamePath();
+            GameTitle title = new GameTitle();
+
+            this.Name = name.NameRTWALX;
+            this.Path = path.PathRTWALX;
+            this.Type = GameType.GameRTWALX;
+            this.Title = title.TitleRTWALX;
+            this.Size = GetSize();
+            this.Version = GetVersion();
+        }
+    }
+}
