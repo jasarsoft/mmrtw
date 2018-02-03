fb0236af73795b79b3a43f21d479b81fee13ffa2
Edin Jašarević <edinjasar14@gmail.com>
2018-02-03 23:58:39 +0100

    name of icons

---
+++ Rome Total War/Class/Icon/IconName.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Jasarsoft.ModManager.RomeTotalWar
+{
+    internal sealed class IconName
+    {
+        private readonly string iconRTW;
+        private readonly string iconRTWBI;
+        private readonly string iconRTWALX;
+
+
+        public IconName()
+        {
+            this.iconRTW = "rtw.png";
+            this.iconRTWBI = "rtwbi.png";
+            this.iconRTWALX = "rtwalx.png";
+        }
+
+
+        public string IconRTW
+        {
+            get { return this.iconRTW; }
+        }
+
+        public string IconRTWBI
+        {
+            get { return this.iconRTWBI; }
+        }
+
+        public string IconRTWALX
+        {
+            get { return this.iconRTWALX; }
+        }
+    }
+}
