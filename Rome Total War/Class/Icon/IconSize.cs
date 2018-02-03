9332968c78ac839bb5c470a2f365bcf6a15036ba
Edin Jašarević <edinjasar14@gmail.com>
2018-02-03 23:59:17 +0100

    size of icons

---
+++ Rome Total War/Class/Icon/IconSize.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Jasarsoft.ModManager.RomeTotalWar
+{
+    internal sealed class IconSize
+    {
+        private readonly int iconWidth;
+        private readonly int iconHeight;
+
+
+        public IconSize()
+        {
+            this.iconWidth = 48;
+            this.iconHeight = 48;
+        }
+
+
+        public int Width
+        {
+            get { return this.iconWidth; }
+        }
+
+        public int Height
+        {
+            get { return this.iconHeight; }
+        }
+    }
+}
