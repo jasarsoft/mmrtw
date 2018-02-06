97fbd2ff0765f86d4952afef51008f44470f7ae4
Edin Jašarević <edinjasar14@gmail.com>
2018-02-06 16:07:17 +0100

    mod platform enum

---
+++ Rome Total War/Class/Mod/ModPlatform.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Jasarsoft.ModManager.RomeTotalWar
+{
+    enum ModPlatform
+    {
+        RTW,    //Rome Total War
+        BI,     //Rome Total War - Barbarian Invasion
+        ALX     //Rome Total War - Alexander
+    };
+}
