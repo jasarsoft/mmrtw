9f8e9df10d134633f0a36839565154daa54f486c
Edin Jašarević <edinjasar14@gmail.com>
2018-02-06 16:38:43 +0100

    mod compaign class

---
+++ Rome Total War/Class/Mod/ModCompaign.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Jasarsoft.ModManager.RomeTotalWar
+{
+    internal sealed class ModCompaign
+    {
+        private string modName;
+        private CompaignType modType;
+
+
+        public enum CompaignType
+        {
+            CompaingNo,
+            CompaingYes
+        };
+
+
+        public ModCompaign(CompaignType type, string name)
+        {
+            this.modType = type;
+            this.modName = name;
+        }
+
+        public string Name
+        {
+            get { return this.modName; }
+        }
+
+        public CompaignType Type
+        {
+            get { return this.modType; }
+        }
+
+
+        public override string ToString()
+        {
+            if(this.modType == CompaignType.CompaingNo)
+            {
+                return String.Format("Yes: {0}", this.modName);
+            }
+            else
+            {
+                return String.Format("No: {0}", this.modName);
+            }
+        }
+    }
+}
