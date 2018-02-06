8e015e03fd1ff00f3f5252f032179b8688e2b251
Edin Jašarević <edinjasar14@gmail.com>
2018-02-06 16:10:01 +0100

    skip mods

---
+++ Rome Total War/Class/Mod/ModSkip.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Jasarsoft.ModManager.RomeTotalWar
+{
+    internal sealed class ModSkip
+    {
+        private string[] modNames;
+
+
+        public ModSkip()
+        {
+            modNames = new string[]
+            {
+                "alexander",
+                "bi",
+                "data",
+                "manager",
+                "miles",
+                "preferences",
+                "saves",
+                "temp",
+                "uninstall"
+            };
+        }
+
+
+        public string this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= modNames.Length)
+                    throw new ArgumentOutOfRangeException();
+
+                return modNames[index];
+            }
+        }
+    }
+}
